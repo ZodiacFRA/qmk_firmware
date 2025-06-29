369d27d0de7e8dd70b2eb5fe97758d5de2398e01
tet <2tqdd9wpn@relay.firefox.com>
2025-06-29 15:23:54 +0200

    working (not nkro tho)

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -18,42 +18,45 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP_5x6(
   // left hand
-   KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5, 
-   KC_GRV,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-   KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
-   KC_LSHIFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-                       KC_TAB,  KC_BSLS,
-                               LT(_FN, KC_DEL), KC_LSFT, 
-                               KC_SPC,   KC_LCTRL,
-                               KC_ENTER, KC_LALT,
-        // right hand
-                     KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
-                     KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
-                     KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-                     KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
-                                      KC_LBRC, KC_RBRC,
-        KC_SPC, LT(_FN, KC_BSPC), 
-        CTL_T(KC_PGUP), KC_PGDN,
-        KC_LALT, KC_LGUI),
+   KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,    KC_5, 
+   KC_TAB,    KC_A,    KC_Z,    KC_E,   KC_R,    KC_T,
+   KC_LSHIFT, KC_Q,    KC_S,    KC_D,   KC_F,    KC_G,
+   KC_LCTRL,  KC_GRV,  KC_W,    KC_X,   KC_C,    KC_V,
+                       KC_BSLS, KC_QUOT,
+                                        KC_LGUI, KC_LALT, 
+                                        _______, KC_SPC,
+                                        _______, _______,
+
+                                                                      // right hand
+                                                                                    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_DEL,
+                                                                                    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
+                                                                                    KC_H,    KC_J,   KC_K,    KC_L,    KC_M,    KC_MINS, 
+                                                                                    KC_B,    KC_N,   KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
+                                                                                                     KC_LBRC, KC_RBRC,
+                                                                      KC_BSPC, LT(_FN, KC_ENTER), 
+                                                                      KC_SPC,  KC_A,
+                                                                      KC_B, KC_C),
+
 [_FN] = KEYMAP_5x6(
   // left hand
-   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5, 
-   _______,  _______,  KC_HOME,  KC_UP,    KC_END,   KC_PGUP,
-   _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGDN,
-   RESET,    _______,  _______,  _______,  _______,  _______,
-                       _______,  _______, 
-                               LT(_FN, KC_DEL), KC_LSFT, 
-                               KC_SPC,   KC_LCTRL,
-                               KC_ENTER, KC_LALT,
-        // right hand
-                     KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,
-                     KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  KC_F12,
-                     KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______, _______,
-                     KC_CALC,  KC_MAIL,  KC_MYCM,  KC_WHOM,  _______,   RESET,
-                                         _______,  _______,
-        KC_SPC, LT(_FN, KC_BSPC), 
-        CTL_T(KC_PGUP), KC_PGDN,
-        KC_LALT, KC_LGUI),
+   _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5, 
+   KC_CAPS, KC_VOLU, KC_HOME,  KC_UP,   KC_END,   KC_PGUP,
+   _______, KC_VOLD, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_PGDN,
+   RESET,   KC_MUTE, _______,  _______, _______,  _______,
+                     _______,  _______,
+                                        KC_LGUI,  KC_LALT, 
+                                        _______,  KC_SPC,
+                                        _______,  _______,
+
+                                                                      // right hand
+                                                                                    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,
+                                                                                    KC_MNXT,  KC_HOME,  KC_UP,    KC_END,   _______,  KC_F12,
+                                                                                    KC_MPRV,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______, _______,
+                                                                                    KC_MPLY,  _______,  _______,  _______,  _______,   RESET,
+                                                                                                        _______,  _______,
+                                                                      KC_BSPC, LT(_FN, KC_ENTER), 
+                                                                      KC_SPC,  KC_A,
+                                                                      KC_B, KC_C),
 };
 
 
