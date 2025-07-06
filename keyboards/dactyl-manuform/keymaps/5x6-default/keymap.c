14681d57312ab4142f09c30557471d860ee850ca
tet <2tqdd9wpn@relay.firefox.com>
2025-07-06 21:03:00 +0200

    added gaming layer

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -10,6 +10,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _FN   1
+#define _GAMING 2
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -35,7 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                      KC_LBRC, KC_RBRC,
                                                                       KC_BSPC, LT(_FN, KC_ENTER), 
                                                                       KC_SPC,  KC_A,
-                                                                      KC_B, KC_C),
+                                                                      KC_B, KC_C
+),
 
 [_FN] = KEYMAP_5x6(
   // left hand
@@ -52,11 +54,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                     KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,
                                                                                     KC_MNXT,  KC_HOME,  KC_UP,    KC_END,   _______,  KC_F12,
                                                                                     KC_MPRV,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______, _______,
-                                                                                    KC_MPLY,  _______,  _______,  _______,  _______, _______,
+                                                                                    KC_MPLY,  _______,  _______,  _______,  _______, TG(_GAMING),
                                                                                                         _______,  RESET,
                                                                       KC_BSPC, LT(_FN, KC_ENTER), 
                                                                       KC_SPC,  KC_A,
-                                                                      KC_B, KC_C),
+                                                                      KC_B, KC_C
+),
+[_GAMING] = KEYMAP_5x6( // Made for games with non rebindable keymap
+  // left hand
+   KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,    KC_5, 
+   KC_TAB,    KC_T,    KC_Z,    KC_W,   KC_R,    KC_F,
+   KC_LSHIFT, KC_Q,    KC_A,    KC_S,   KC_D,    KC_G,
+   KC_LCTRL,  KC_GRV,  KC_LALT, KC_X,   KC_C,    KC_V,
+                       KC_BSLS, KC_QUOT,
+                                        KC_LGUI, KC_E, 
+                                        _______, KC_SPC,
+                                        _______, _______,
+
+                                                                      // right hand
+                                                                                    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_DEL,
+                                                                                    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
+                                                                                    KC_H,    KC_J,   KC_K,    KC_L,    KC_M,    KC_MINS, 
+                                                                                    KC_B,    KC_N,   KC_COMM, KC_DOT,  KC_SLSH, TG(_GAMING),
+                                                                                                     KC_LBRC, KC_RBRC,
+                                                                      KC_BSPC, LT(_FN, KC_ENTER), 
+                                                                      KC_SPC,  KC_A,
+                                                                      KC_B, KC_C
+)
 };
 
 
